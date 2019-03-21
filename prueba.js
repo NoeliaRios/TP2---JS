316f1f654e99cab25c3b930b97ec771f375a20ff
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-03-20 21:40:33 -0300

    1D

--- prueba.js
+++ prueba.js
@@ -22,7 +22,7 @@ var local = {
     ]
 };
 
-//EJERCICIO 1. precioMaquina(componentes): recibe un array de componentes y devuelve el precio de la máquina que se puede
+//EJERCICIO 1A. precioMaquina(componentes): recibe un array de componentes y devuelve el precio de la máquina que se puede
 //armar con esos componentes, que es la suma de los precios de cada componente incluido.
 
 function precioMaquina(componentes) {
@@ -48,7 +48,7 @@ console.log(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"]));
 
 
 
-//EJERCICIO 2. cantidadVentasComponente(componente): recibe un componente y devuelve la cantidad de veces que fue vendido, o 
+//EJERCICIO 1B. cantidadVentasComponente(componente): recibe un componente y devuelve la cantidad de veces que fue vendido, o 
 //sea que formó parte de una máquina que se vendió. La lista de ventas no se pasa por parámetro, se asume que está 
 //identificada por la variable ventas.
 
@@ -70,3 +70,67 @@ function cantidadVentasComponente(componente) {
 }
 
 console.log(cantidadVentasComponente("Monitor ASC 543")); // 2
+
+
+
+//EJERCICIO 1C. vendedoraDelMes(mes, anio), se le pasa dos parámetros numéricos, (mes, anio) y devuelve el nombre de la vendedora 
+// que más vendió en plata en el mes. O sea no cantidad de ventas, sino importe total de las ventas. El importe de una venta es el 
+// que indica la función precioMaquina.
+
+function vendedoraDelMes(mes, anio) {
+
+    var nuevaVarParaGuardarcomponentes = 0;
+    var ventasAda = 0;
+    var ventasGrace = 0;
+
+    for (var i = 0; i < local.ventas.length; i++) {
+        //console.log(local.ventas[i].fecha.getMonth() + 1)
+        //console.log(mes)
+        //console.log(local.ventas[i].fecha.getFullYear())
+
+        if (local.ventas[i].fecha.getMonth() + 1 == mes && local.ventas[i].fecha.getFullYear() == anio) {
+            //console.log(precioMaquina(local.ventas[i].componentes))
+     
+
+        if (local.ventas[i].nombreVendedora == "Ada") {
+            //console.log(precioMaquina(local.ventas[i].componentes))
+            ventasAda = ventasAda + precioMaquina(local.ventas[i].componentes)
+        }
+
+        if (local.ventas[i].nombreVendedora == "Grace") {
+            //console.log(precioMaquina(local.ventas[i].componentes))
+            ventasGrace = ventasGrace + precioMaquina(local.ventas[i].componentes)
+        }
+    }
+
+    } if (ventasAda < ventasGrace) {
+        console.log("Gano Ada con " + ventasAda)
+        return 'Ada'
+    }
+    else if (ventasAda > ventasGrace) {
+        console.log("gano grace con " + ventasGrace)
+        return "grace"
+    }
+}
+
+console.log(vendedoraDelMes(1, 2019)); // "Ada" (vendio por $670, una máquina de $320 y otra de $350)
+
+
+//EJERCICIO 1D. ventasMes(mes, anio): Obtener las ventas de un mes.
+
+function ventasMes(mes, anio) {
+
+    var ventasTotal = 0
+
+    for (var i = 0; i < local.ventas.length; i++) {       
+
+        if (local.ventas[i].fecha.getMonth() + 1 == mes && local.ventas[i].fecha.getFullYear() == anio) {
+            //console.log(precioMaquina(local.ventas[i].componentes))
+            ventasTotal = ventasTotal + precioMaquina(local.ventas[i].componentes)            
+        }
+    }
+    return ventasTotal
+}
+
+console.log( ventasMes(1, 2019) ); // 1250
+
